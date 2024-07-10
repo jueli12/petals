0ca01b01e54b15e68f02eeda7b1a8fd98fabb239
jueli12 <juelidlam@gmail.com>
2024-07-10 13:13:28 +0800

    test: num_blocks

--- src/petals/server/server.py
+++ src/petals/server/server.py
@@ -320,6 +320,7 @@ class Server:
 
         num_blocks = min(num_blocks, self.block_config.num_hidden_layers)
         logger.info(
+            f"Total Memory is {total_memory}, num_devices is {num_devices}, tensor_parallel_devices is {self.tensor_parallel_devices}."
             f"Server will fill your GPU memory with {num_blocks} transformer blocks. "
             f"If you want to leave some free GPU memory, please specify a lesser --num_blocks manually"
         )
